1dfcee6d7294a098c2b1fc4978a7fba7429eb49d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-16 07:17:28 +0200

    create viewRegisteredCourses function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CourseRegistration;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->middleware('role:user');
+    }
+
+    // Allows a student to view the courses they are registered for
+    public function viewRegisteredCourses()
+    {
+        $studentID = auth()->id();
+        $registeredCourses = CourseRegistration::with('course')
+            ->where('StudentID', $studentID)
+            ->get();
+
+        return response()->json([
+            'registeredCourses' => $registeredCourses,
+        ]);
+    }
 }
