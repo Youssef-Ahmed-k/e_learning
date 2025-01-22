f727bd62a7308a66ad986907012bddd8b995069b
youssefkota <104798903+Youssef-Ahmed-k@users.noreply.github.com>
2025-01-22 02:09:25 +0200

    add ResetPasswordRequest class for validating password reset requests

---
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'otp' => 'required|max:4',
+            'password' => 'required|string|min:8|confirmed',
+         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'The email field is required.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.exists' => 'The selected email is invalid.',
+            'otp.required' => 'The token field is required.',
+            'otp.max' => 'The otp must be a 4 numbers.',
+            'password.required' => 'The password field is required.',
+            'password.string' => 'The password must be a string.',
+            'password.min' => 'The password must be at least 8 characters.',
+            'password.confirmed' => 'The password confirmation does not match.',
+        ];
+    }
+}
