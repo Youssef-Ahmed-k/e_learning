fa26bda654d685575410d3e90c1d673fa4206a39
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-27 07:56:09 +0200

    add construct to QuestionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->middleware('role:professor');
+    }
 }
