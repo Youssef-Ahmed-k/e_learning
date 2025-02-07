a10483d3d0de3731f8b7fe2b309f1536860af099
Youssef <youssefahmed8878@icloud.com>
2025-02-07 02:34:14 +0200

    update Questions table

--- database/migrations/2024_11_27_030329_create_questions_table.php
+++ database/migrations/2024_11_27_030329_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id('QuestionID');
             $table->text('Content');
-            $table->enum('Type', ['mcq', 'short_answer', 'true_false'])->default('mcq'); // Question type
+            $table->enum('Type', ['mcq', 'true_false'])->default('mcq'); // Question type
             $table->integer('Marks');
             $table->string('image')->nullable();
             $table->foreignId('QuizID')
