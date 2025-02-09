7cdfdea1442abef1d62bbd8cb1e74a1e9d659a35
Youssef <youssefahmed8878@icloud.com>
2025-02-10 00:02:25 +0200

    add function getUserNotifications to get Notifications

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->middleware('role:user');
+    }
+    public function getUserNotifications()
+    {
+        try {
+            // Get the authenticated user's ID
+            $user_id = auth()->user()->id;
+
+            // Fetch all notifications for the user, sorted by latest
+            $notifications = Notification::where('RecipientID', $user_id)
+                ->orderBy('SendAt', 'desc')
+                ->select('Message', 'SendAt') 
+                ->get();
+
+            return response()->json(['notifications' => $notifications]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Something went wrong', 'message' => $e->getMessage()], 500);
+        }
+    }
+}
