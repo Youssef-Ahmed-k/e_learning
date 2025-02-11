5e67b1047cf487c0bfd0c12f93a3a707b1af6e30
Youssef <youssefahmed8878@icloud.com>
2025-02-11 02:47:14 +0200

    Updated Notification model: added table property, type & CourseID to $fillable, and setSendAtAttribute method.

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,17 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
-    public $timestamps = false;
-    protected $primaryKey = 'NotificationID';
+
+    protected $table = 'notifications'; 
+    protected $primaryKey = 'NotificationID'; 
+
+    public $timestamps = false; 
     protected $fillable = [
         'Message',
         'SendAt',
         'RecipientID',
         'is_read',
+        'type', 
+        'CourseID',
     ];
+    public function setSendAtAttribute($value)
+    {
+        if (!$this->exists) {
+            $this->attributes['SendAt'] = $value;
+        }
+    }
 
     public function recipient()
     {
         return $this->belongsTo(User::class, 'RecipientID', 'id');
+    }
+
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'CourseID', 'CourseID');
     }
 }
