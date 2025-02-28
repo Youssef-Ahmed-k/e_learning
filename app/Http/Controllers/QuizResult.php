99d351fbd6489a5c06e02911f0e5691c51f03857
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-28 07:50:18 +0200

    create __construct function in QuizResult Controller

--- app/Http/Controllers/QuizResult.php
+++ app/Http/Controllers/QuizResult.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class QuizResult extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->middleware('role:professor', ['except' => ['getStudentQuizzes', 'startQuiz', 'submitQuiz', 'getQuizResult', 'getStudentQuizzesWithResults', 'compareStudentAnswers', 'getSubmittedQuizzes']]);
+        $this->middleware('role:user', ['only' => ['getStudentQuizzes', 'startQuiz', 'submitQuiz', 'getQuizResult', 'getStudentQuizzesWithResults', 'compareStudentAnswers', 'getSubmittedQuizzes']]);
+    }
 }
